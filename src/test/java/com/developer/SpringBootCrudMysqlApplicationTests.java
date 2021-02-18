1086afa142c230dc0695a59548122a09b4be84eb
sireesha-sr <sireeshareddy.srd@gmail.com>
2021-02-18 13:46:32 +0530

    My sql crud operations
    Change-Id: Ie6e4fb30e9efc56ee34f6fe1a4e1bff84c40f6b0
    Signed-off-by: sireesha-sr <sireeshareddy.srd@gmail.com>

---
+++ src/test/java/com/developer/SpringBootCrudMysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.developer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCrudMysqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
